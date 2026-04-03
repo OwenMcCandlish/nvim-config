7200d85998d4b0e8f5e78ebbf8687daacb39c3dd
Owen McCandlish <omccandlish@gmail.com>
2026-04-03 16:15:50 -0400

    Local packages and install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+# Install nvim
+mkdir -p ~/.local/bin
+cp ./archive/nvim-linux-x86_64.appimage ~/.local/bin/nvim
+chmod u+x ~/.local/bin/nvim
+
+# Install lazygit
+tar xf archive/lazygit.tar.gz lazygit
+mv lazygit ~/.local/bin/
+
+# Install ripgrep
+tar xf ./archive/ripgrep-15.1.0-x86_64-unknown-linux-musl.tar.gz
+mv ripgrep-15.1.0-x86_64-unknown-linux-musl/rg ~/.local/bin/
+rm -rf ripgrep-15.1.0-x86_64-unknown-linux-musl
+
+# Configure PATH
+if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
+    echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
+fi
+source ~/.bashrc
